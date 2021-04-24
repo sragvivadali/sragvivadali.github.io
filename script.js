e2f899b9075ad37787aa9d6c2ceb55f13d6d0ef4
sragvivadali <58544815+sragvivadali@users.noreply.github.com>
2021-04-24 15:19:02 -0700

    Add files via upload
    This is my first commit

---
+++ script.js
@@ -0,0 +1,159 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal('.work__img', { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input', { interval: 200 });
+
+
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel__item');
+const totalSlides = slides.length;
+
+document.
+    getElementById('carousel__button--next')
+    .addEventListener("click", function () {
+        moveToNextSlide();
+    });
+document.
+    getElementById('carousel__button--prev')
+    .addEventListener("click", function () {
+        moveToPrevSlide();
+    });
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel__item--visible');
+        slide.classList.add('carousel__item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+$(document).ready(function () {
+    $(".button").click(function () {
+        var name = $(this).attr("data-filter");
+        if (name === "all") {
+            $(".filter").show("2000");
+        }
+        else {
+            $(".filter").not("." + name).hide("2000");
+            $(".filter").filter("." + name).show("2000");
+        }
+    });
+    $(".navigation a").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+});
