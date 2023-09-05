a2f7bfd332906e130831af8810030f5ab37c4355
Sragvi Vadali <reachsragvi@gmail.com>
2023-09-05 11:16:24 -0700

    fixing resume link

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
   name: 'Sragvi Vadali',
   role: 'UCLA Computer Science & Linguistics Student',
   resume:
-    'https://docs.google.com/document/d/1ADJih2r58tAKmFBAzYE7RDpnxW_NyTgBM5j5p4DZ9VQ/edit?usp=sharing',
+    'https://drive.google.com/file/d/1vkUnjNc7VZljxs7bh5lVUJEWznjBXBbf/view?usp=sharing',
   description:
     "Hello! I'm a passionate student, and I'm on track to graduate in the class of 2025. My primary interests lie in the captivating worlds of computer science and natural language processing (NLP). In addition to my technical pursuits, I have a creative side that I express through photography. My ultimate goal is to hone my skills in AI development and NLP to undertake projects that make a significant impact, pushing the boundaries of innovation and problem-solving.",
   social: {
